c815ef757b46a854fdccffe6e2e3b96ad711b60b
Brynner03 <bventura03@hotmail.com>
2022-06-20 01:23:54 -0400

    Added Switch hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Welcome from './pages/Welcome'
 import Products from './pages/Products'
@@ -10,15 +10,17 @@ function App() {
     <div>
       <MainHeader />
       <main>
+       <Switch>
         <Route path='/welcome'>
           <Welcome />
         </Route>
-        <Route path='/products'>
+        <Route path='/products' exact>
           <Products />
         </Route>
         <Route path='/products/:productId'>
           <ProductDetails />
         </Route>
+      </Switch> 
       </main>
     </div>
   );
